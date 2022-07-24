c555ccd86d119070f660dcc11ce9a7ec58c81f2a
Zion Miller <djzionmiller@gmail.com>
2022-07-24 15:45:36 -0700

    commit tree 696a0d2e2f7a432cfce1380d7139127613f0d71e

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+const [isInCart, setIsInCart] = useState(false)
+
+// const addedToCart = "is in cart" ? "Remove From Cart" : "Add to Cart"
+  
+function handleShoppingCart() {
+  // console.log("got clicked")
+  setIsInCart((isInCart) => !isInCart)
+  
+}
+
   return (
-    <li className="">
+    <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={isInCart ? "Remove" : "Add"} onClick={handleShoppingCart}>
+        {isInCart ? "Remove From" : "Add To"} Cart
+      </button>
     </li>
   );
 }
